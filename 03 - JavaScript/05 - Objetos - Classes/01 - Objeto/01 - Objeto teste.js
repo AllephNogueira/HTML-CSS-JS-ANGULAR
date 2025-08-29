d04204e7bc26e2d5c684da495853960527fcd425
Alleph <allephn@hotmail.com.br>
2025-08-29 17:04:11 -0300

    isFinito em JavaScript e também codigo para descontar valor de impostos do salario.

--- 03 - JavaScript/05 - Objetos - Classes/01 - Objeto/01 - Objeto teste.js
+++ 03 - JavaScript/05 - Objetos - Classes/01 - Objeto/01 - Objeto teste.js
@@ -1,3 +1,11 @@
+
+/** Diferença de objetos para Classe
+ * Repara que nesse exemplo aqui o objeto, ja temos os dados definidos nele
+ * Classe é um modelo de objeto, mas que ainda nao tem os dados, vamos precisar definir eles ainda
+ *  Usando por exemplo um construtor
+ *  Usando por meio de um new e depois adicionando os itens.
+ */
+
 const usuario = {
     nome : 'Alleph',
     sobrenome : 'Nogueira',
--- 03 - JavaScript/05 - Objetos - Classes/05 - Construtor/01 - Construtor.js
+++ 03 - JavaScript/05 - Objetos - Classes/05 - Construtor/01 - Construtor.js
@@ -1,61 +1,70 @@
-    class Carro {
-        marca;
-        modelo;
 
-        constructor() {
-            this.marca = 'GM' // COLOCANDO UM PADRAO AQUI
-            this.modelo = 'Onix'
-        }
+/** Diferença de objetos para Classe
+ * Repara que nesse exemplo aqui o objeto, ja temos os dados definidos nele
+ * Classe é um modelo de objeto, mas que ainda nao tem os dados, vamos precisar definir eles ainda
+ *  Usando por exemplo um construtor
+ *  Usando por meio de um new e depois adicionando os itens.
+ */
+
+
+class Carro {
+    marca;
+    modelo;
+
+    constructor() {
+        this.marca = 'GM' // COLOCANDO UM PADRAO AQUI
+        this.modelo = 'Onix'
     }
+}
 
-    const carro1 = new Carro()
-    console.log(carro1)
+const carro1 = new Carro()
+console.log(carro1)
 
 // Agora obrigando a passarem os dados no construtor
 // Sempre quando uma pessoa for instanciada, devemos passar os atributos.
-    class Carro2 {
-        marca;
-        modelo;
+class Carro2 {
+    marca;
+    modelo;
 
-        constructor(marca, modelo) {
-            this.marca = marca
-            this.modelo = modelo
-        }
+    constructor(marca, modelo) {
+        this.marca = marca
+        this.modelo = modelo
     }
+}
 
-    const carro2 = new Carro2('Ford', 'Fiesta')
-    console.log(carro2)
+const carro2 = new Carro2('Ford', 'Fiesta')
+console.log(carro2)
 
 // Deixando sem dados
 // Dessa forma colocamos um padrao, ou vai ser a variavel, ou vai ser o vazio.
-    class Carro3 {
-        marca;
-        modelo;
+class Carro3 {
+    marca;
+    modelo;
 
-        constructor(marca = '', modelo = '') {
-            this.marca = marca
-            this.modelo = modelo
-        }
+    constructor(marca = '', modelo = '') {
+        this.marca = marca
+        this.modelo = modelo
     }
+}
 
-    const carro3 = new Carro3('Ford', 'Fiesta')
-    console.log(carro3)
+const carro3 = new Carro3('Ford', 'Fiesta')
+console.log(carro3)
 
 // Fazendo uma conta direto no construtor
-    class Carro4 {
-        marca;
-        modelo;
-        anoCarro;
-        idadeCarro;
+class Carro4 {
+    marca;
+    modelo;
+    anoCarro;
+    idadeCarro;
 
-        constructor(marca = '', modelo = '', anoCarro) {
-            this.marca = marca
-            this.modelo = modelo
-            this.anoCarro = anoCarro
-            this.idadeCarro = 2025 - this.anoCarro // FAZENDO UMA CONTA DIRETO NO CONSTRUTOR.
-        }
+    constructor(marca = '', modelo = '', anoCarro) {
+        this.marca = marca
+        this.modelo = modelo
+        this.anoCarro = anoCarro
+        this.idadeCarro = 2025 - this.anoCarro // FAZENDO UMA CONTA DIRETO NO CONSTRUTOR.
     }
+}
 
-    const carro4 = new Carro4('Citroen', 'C4', 2016)
-    console.log(carro4)
+const carro4 = new Carro4('Citroen', 'C4', 2016)
+console.log(carro4)
 
